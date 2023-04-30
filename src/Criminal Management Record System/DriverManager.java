3e7e3bd1d4d54d1c9b18ae57fd1811a1e6b8ff5b
unknown <eveeinstein90@gmail.com>
2023-04-30 11:01:28 +0000

    first commit

---
+++ src/Criminal Management Record System/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inventorymanagementsystem;
+
+import com.sun.jdi.connect.spi.Connection;
+
+/**
+ *
+ * @author Sabina B. Heyford
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306provisions, String root, String evanseinstein90) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
